e07ac6b4450784448afa8c248855805aed67f497
carina <115407229+t14c4r1n4@users.noreply.github.com>
2024-05-18 10:23:45 +0000

    changed directory to vite

---
+++ vite/src/assets/images/index.js
@@ -0,0 +1,52 @@
+// languages
+
+import de from './languages/de.svg'
+import en from './languages/gb.svg'
+import es from './languages/es.svg'
+
+
+// company icons
+
+import bwr from './companies/bwr.png'
+import guggenberger from './companies/guggenberger.jpg'
+import lra from './companies/lra.png'
+import nyf from './companies/nyf.png'
+import wittmann from './companies/wittmann.png'
+import wuerth from './companies/wuerth.png'
+import zoll from './companies/zoll.png'
+import klett from './companies/klett.png'
+import wbs from './companies/wbs.png'
+import db from './companies/db.svg'
+
+// background
+import background from './others/background.png'
+import bg from './bg.svg'
+import bg_concrete from './bg_concrete.jpg'
+import hue_bg from './hue_bg.jpg'
+
+// me
+import me from './others/me.png'
+import me_circle from './me_circle.png'
+
+export {
+    de,
+    en,
+    es,
+    bwr,
+    guggenberger,
+    lra,
+    nyf,
+    wittmann,
+    wuerth,
+    zoll,
+    klett,
+    wbs,
+    background,
+    me,
+    me_circle,
+    db,
+    bg, 
+    hue_bg
+}
+
+export default bg;
---
+++ vite/src/assets/sections/technologies.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { technologies } from "../constants/tech";
+
+export default function Tech() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const icons = technologies.map((techImage, index) => (
+    <article
+      className="flex relative place-content-center"
+      key={index}
+      onMouseEnter={() => setActiveIndex(index)}
+      onMouseLeave={() => setActiveIndex(null)}
+      onClick={() => setActiveIndex(index)}
+    >
+      <div className="p-2 transition hover:scale-115 duration-300 group cursor-pointer hex glass flex">
+        {techImage.icon}
+      </div>
+
+      {activeIndex === index && (
+        <span className="-translate-y-16 -translate-x-8 absolute text-zinc-200 mx-5 align-center justify-center z-10 bg-zinc-600 rounded-lg p-3 overflow-auto">
+          {techImage.name}
+        </span>
+      )}
+    </article>
+  ));
+
+  return (
+    <div className="flex flex-wrap justify-center gap-2 p-5 rounded-2xl max-w-[40vw]">
+      {icons}
+    </div>
+  );
+}
